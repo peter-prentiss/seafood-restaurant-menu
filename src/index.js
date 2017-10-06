45ad9cd8cbada55faaee5c68b6b9dfed62f85239
Peter <peter.prentiss@gmail.com>
2017-10-06 18:39:03 -0500

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './css/style.css';
+
+import StorePicker from './components/StorePicker';
+
+render(
+  <StorePicker />,
+  document.getElementById('main')
+);
