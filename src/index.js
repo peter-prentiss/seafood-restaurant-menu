f3f4e7a59fce24c9533a006044f5193df8c6508a
Peter <peter.prentiss@gmail.com>
2017-10-13 21:46:08 -0500

    Add App, Header, Order and Inventory components.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import './css/style.css';
 
+import App from './components/App';
 import StorePicker from './components/StorePicker';
 
 render(
-  <StorePicker />,
+  <App />,
   document.getElementById('main')
 );
