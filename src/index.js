b89805b4cebf5554015ff2dcf7ac9b0c44a7da1b
Peter <peter.prentiss@gmail.com>
2017-10-17 14:06:18 -0500

    Implement react router.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './css/style.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import './css/style.css';
 import App from './components/App';
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component ={StorePicker} />
+        <Route path="/store/:storeId" component ={App} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
 
 render(
-  <App />,
+  <Root />,
   document.getElementById('main')
 );
