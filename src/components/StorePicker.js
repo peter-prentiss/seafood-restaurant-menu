3d6619b4e7827182dbc66102ffd85831a2e8e574
Peter <peter.prentiss@gmail.com>
2017-10-19 22:57:25 -0500

    Add firebase.

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
+import { getFunName } from '../helpers';
+import PropTypes from 'prop-types';
 
 class StorePicker extends Component {
+  // constructor() {
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+
+  goToStore(e) {
+    console.log('You changed the url');
+    e.preventDefault();
+    const storeId = this.storeInput.value;
+    console.log(`Going to ${storeId}`);
+    this.props.history.push(`/store/${storeId}`);
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form onSubmit={(e) => this.goToStore(e)} className="store-selector">
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input }}/>
         <button type="submit">Visit Store</button>
       </form>
     )
   }
+}
+
+StorePicker.propTypes = {
+  router: PropTypes.object
 }
 
 export default StorePicker;
